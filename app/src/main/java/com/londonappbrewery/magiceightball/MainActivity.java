6038437d1620cf270203841c3d8ba7b9d6d9ad1b
samirsumanjha <samirsumanjha@gmail.com>
2018-06-28 21:04:43 +0530

    Magic Ball assignment complete !!!

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         final ImageView ballDisplay= findViewById(R.id.image_eightBall);
         Button myButton= findViewById(R.id.askButton);
-        final int[] ballArray= {R.drawable.ball1,R.drawable.ball2,R.drawable.ball3,R.drawable.ball4,R.drawable.ball5};
+        final int[] ballArray= new int[] {R.drawable.ball1,R.drawable.ball2,R.drawable.ball3,R.drawable.ball4,R.drawable.ball5};
         myButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
